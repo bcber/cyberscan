5eec0fe78381f936f1fa7d9339d90e52af61860f
Dionne Saunders <linx@castlemako.com>
2014-05-11 15:28:19 +1000

    replaced netstat with the first scan, nmap. disabed buttons on index page if scanning in progress.

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,13 @@
 class Target < ActiveRecord::Base
   attr_accessible :ip_address, :name, :report_id, :status
   has_one :report
+
+  def print_status
+    if self.status == 'scanning'
+      "<font size='3' color='red'>#{self.status.titleize}</font>"
+    elsif self.status == 'scanned'
+      "<font size='3' color='green'>#{self.status.titleize}</font>"
+    end
+  end
+
 end
--- app/workers/scan_worker.rb
+++ app/workers/scan_worker.rb
@@ -9,9 +9,10 @@ class ScanWorker
   # To start with just doing a quick test with netstat
   def perform(target_id)
     @target = Target.find_by_id target_id
-    puts "Doing stuff with #{@target.ip_address}"
+    puts "RUNNING COMMAND: nmap -vv -Pn -A -sC -sS -T 4 -p- #{@target.ip_address}"
 
-    cmd = "netstat -an"
+    # -sS not always allowed without root. this scan is therefore using -sT and is not as stealthy as it could be
+    cmd = "nmap -vv -Pn -A -sC -sT -T 4 -p- #{@target.ip_address}"
     the_output = ""
     Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
       stdout.read.split("\n").each do |line|
@@ -19,7 +20,7 @@ class ScanWorker
       end
     end
     the_output = the_output.gsub('\n','<BR>')
-    @report = Report.create!(:tool => 'netstat -an', :tool_output => the_output, :target_id => @target.id)
+    @report = Report.create!(:tool => 'nmap -vv -Pn -A -sC -sT -T 4 -p- ', :tool_output => the_output, :target_id => @target.id)
     @target.update_attribute(:status,'scanned')
 
 
